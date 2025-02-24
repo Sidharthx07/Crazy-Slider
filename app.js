e3addfc6e97cdb185ecde6e3b93b788767b19c0b
Sidharthx07 <sidharth01032021@gmail.com>
2025-02-24 17:02:37 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,62 @@
+// Step 1: Get DOM elements
+let nextDom = document.getElementById('next');
+let prevDom = document.getElementById('prev');
+
+let carouselDom = document.querySelector('.carousel');
+let SliderDom = carouselDom.querySelector('.carousel .list');
+let thumbnailBorderDom = document.querySelector('.carousel .thumbnail');
+let thumbnailItemsDom = thumbnailBorderDom.querySelectorAll('.item');
+let timeDom = document.querySelector('.carousel .time');
+
+thumbnailBorderDom.appendChild(thumbnailItemsDom[0]);
+
+let timeRunning = 3000;
+let timeAutoNext = 7000;
+let isSliding = false;  // Prevent fast clicking issues
+
+nextDom.onclick = function () {
+    if (!isSliding) {
+        showSlider('next');
+    }
+};
+
+prevDom.onclick = function () {
+    if (!isSliding) {
+        showSlider('prev');
+    }
+};
+
+let runTimeOut;
+let runNextAuto = setTimeout(() => {
+    nextDom.click();
+}, timeAutoNext);
+
+function showSlider(type) {
+    if (isSliding) return;
+    isSliding = true;
+
+    let SliderItemsDom = SliderDom.querySelectorAll('.carousel .list .item');
+    let thumbnailItemsDom = document.querySelectorAll('.carousel .thumbnail .item');
+
+    if (type === 'next') {
+        SliderDom.appendChild(SliderItemsDom[0]);
+        thumbnailBorderDom.appendChild(thumbnailItemsDom[0]);
+        carouselDom.classList.add('next');
+    } else {
+        SliderDom.prepend(SliderItemsDom[SliderItemsDom.length - 1]);
+        thumbnailBorderDom.prepend(thumbnailItemsDom[thumbnailItemsDom.length - 1]);
+        carouselDom.classList.add('prev');
+    }
+
+    clearTimeout(runTimeOut);
+    runTimeOut = setTimeout(() => {
+        carouselDom.classList.remove('next');
+        carouselDom.classList.remove('prev');
+        isSliding = false; // Re-enable clicks after animation
+    }, timeRunning);
+
+    clearTimeout(runNextAuto);
+    runNextAuto = setTimeout(() => {
+        nextDom.click();
+    }, timeAutoNext);
+}
